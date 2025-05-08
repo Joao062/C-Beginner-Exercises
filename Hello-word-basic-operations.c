f3d3f32a8481783ae59b56bf4b9d82daba25be23
João Victor <joaovitorlopes062@gmail.com>
2025-05-07 22:37:14 -0300

    Adding first exercise

---
+++ Hello-word-basic-operations.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int idade = 25;
+    float altura = 1.75;
+    
+    printf("Olá Mundo!\n");
+    printf("Minha idade é %d\n", idade);
+    printf("Minha altura é %.2f\n", altura);
+
+    return 0;
+}
